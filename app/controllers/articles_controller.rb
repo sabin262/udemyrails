76c35567f91e6a0fae75b59ed827095abc910f30
sabin262 <sabin262@gmail.com>
2018-07-11 11:19:52 +0545

    Edit Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,23 +3,39 @@ class ArticlesController < ApplicationController
 		@article = Article.new 
 	end
 
-	def create 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+
+	def create #used for new
 		 @article = Article.new(article_params)#cannot directly send through params so method used
 		 if @article.save
 		 	flash[:notice] = "Article Saved!!"
-		 	redirect_to article_path(@article)
+		 	redirect_to article_path(@article)#If article is saved then redirect to article_path ie show.html.erb
 		 else
 		 	render 'new'
 		 end
 	end
 
-	def show 
+	def update #used for edit
 		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			flash[:notice] = "Article Updated"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
+	end
+
+
+	def show 
+		@article = Article.find(params[:id])#use id to show the article that was just saved
 	end
 
 	private
 		def article_params
-			params.require(:article).permit(:title, :description)		
+			params.require(:article).permit(:title, :description)#whitelist the params		
 		end
  
 end
