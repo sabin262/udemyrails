6f455ea3095b60c37ffcd2dbca3f522c30ecd0e4
sabin262 <sabin262@gmail.com>
2018-07-12 14:17:05 +0545

    Styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,9 @@ class ArticlesController < ApplicationController
 
 
 	def create #used for new
+	     #debugger
 		 @article = Article.new(article_params)#cannot directly send through params so method used
+		 @article.user = User.first
 		 if @article.save
 		 	flash[:success] = "Article Saved!!"
 		 	redirect_to article_path(@article)#If article is saved then redirect to article_path ie show.html.erb
